b7a7b191a323d293e80d446a1297fe62ca391fcd
marcelo-landivar <marcelolandivar24@gmail.com>
2026-02-05 15:33:35 +0100

    [ci] Add CI files

--- src/App.test.js
+++ src/App.test.js
@@ -6,10 +6,3 @@ test('renders learn react link', () => {
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
-
-
-test('renders without crashing', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
